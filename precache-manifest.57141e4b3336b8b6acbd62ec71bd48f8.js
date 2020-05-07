6efa1b244b1e1023495386b25d9a8002f2f718be
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-05-07 10:14:22 -0700

    Updates

---
+++ precache-manifest.57141e4b3336b8b6acbd62ec71bd48f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47af4bd22a4c08ebf43f1d55630c3ddc",
+    "url": "/acdb/index.html"
+  },
+  {
+    "revision": "d6e9eda795c790ec9c03",
+    "url": "/acdb/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b5c22a71cc82c2507be",
+    "url": "/acdb/static/js/2.ba053b55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acdb/static/js/2.ba053b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e9eda795c790ec9c03",
+    "url": "/acdb/static/js/main.91e86d54.chunk.js"
+  },
+  {
+    "revision": "60bb976727f65623643f",
+    "url": "/acdb/static/js/runtime-main.5ddfbd2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acdb/static/media/logo.5d5d9eef.svg"
+  }
+]);
